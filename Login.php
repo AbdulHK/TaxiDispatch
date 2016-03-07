c323e629e95d514623cd4b3c969a90a63052b675
Abdulellah Hakim <D12122837@mydit.ie>
2016-03-07 11:53:40 +0000

    fixed login

--- Login.php
+++ Login.php
@@ -9,9 +9,9 @@
 <!-- Optional Bootstrap theme -->
 <link rel="stylesheet" href="css/bootstrap-theme.min.css">
 </head>
-<div class="container">
   <header class="navbar-inverse" role="banner">
-    <div class="container">
+  <div class="row">
+  <div class=".col-med-4">
   <navrole="navigation">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
@@ -23,36 +23,17 @@
       </button>
       <a class="navbar-brand" href="#">Register</a>
     </div>
-
+    </navrole="navigation">
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
         <li><a href="#">Login <span class="sr-only">(current)</span></a></li>
         <li><a href="#">Book</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Register <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Register as dispatcher</a></li>
-            <li><a href="#">Register as Customer</a></li>
-          </ul>
-        </li>
+        <li><a href="#">Book</a></li> 
       </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Menu <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Book</a></li>
-            <li><a href="#">Register as customer</a></li>
-            <li><a href="#">Register as Dispatcher</a></li>
-            <li><a href="#">Book A Taxi</a></li>
-            <li><a href="#">About Us</a></li>
-            <li><a href="#">Register</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div><!-- /.navbar-collapse -->
+      </div>
+      </div>
 </nav>
-</div>
 </header>
 </div>
 <body>
@@ -78,52 +59,7 @@
 </div>
 </div>
 </div>
-<div class="container">
-  <header class="navbar-inverse" role="banner">
-    <div class="container">
-  <navrole="navigation">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">Register</a>
-    </div>
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li><a href="#">Login <span class="sr-only"></span></a></li>
-        <li><a href="#">Book</a></li>
-        <li class="dropup">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Register <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Register as dispatcher</a></li>
-            <li><a href="#">Register as Customer</a></li>
-          </ul>
-        </li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li class="dropup ">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Menu <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Book</a></li>
-            <li><a href="#">Register as customer</a></li>
-            <li><a href="#">Register as Dispatcher</a></li>
-            <li><a href="#">Book A Taxi</a></li>
-            <li><a href="#">About Us</a></li>
-            <li><a href="#">Register</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div><!-- /.navbar-collapse -->
-</nav>
-</div>
-</header>
-</div>
 <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
 </body>
