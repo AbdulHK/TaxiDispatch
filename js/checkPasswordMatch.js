4963303b65fcc4b5fa5ce7f4e7d5329c6e18cffc
Abdulellah Hakim <D12122837@mydit.ie>
2016-03-16 23:32:39 +0000

    code clean up

--- js/checkPasswordMatch.js
+++
@@ -1,9 +0,0 @@
-function checkPasswordMatch() {
-    var password = $("#inputpassword").val();
-    var confirmPassword = $("#inputconfirmpassword").val();
-
-    if (password != confirmPassword)
-        $("#divCheckPasswordMatch").html("Passwords do not match!");
-    else
-        $("#divCheckPasswordMatch").html("Passwords match.");
-}
