d88be00fcc89b0fa8a65696037d3d450041f965f
Abdulellah Hakim <D12122837@mydit.ie>
2016-04-13 19:34:32 +0100

    clean up

--- book1.php
+++ book1.php
@@ -1,143 +1,6 @@
 <?php
-session_start();
-                        if ( isset($_SESSION["error"]) ) 
-                            {  
-                            unset($_SESSION["error"]); 
-                            } 
-                            if ( isset($_SESSION["success"]) ) 
-                            { 
-                             $_SESSION["success"];
-
-                            }
-                            if ( isset($_SESSION["custsucess"]) ) 
-                            { 
-
-                            $_SESSION["custsucess"];
-                            }  
-
+require_once("navbar.php")
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Creative - Start Bootstrap Theme</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
-
-    <!-- Custom Fonts -->
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css" type="text/css">
-
-    <!-- Plugin CSS -->
-    <link rel="stylesheet" href="css/animate.min.css" type="text/css">
-
-    <!-- Custom CSS -->
-    <link rel="stylesheet" href="css/creative.css" type="text/css">
-
-</head>
-
-<body id="page-top">
-
-    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
-        <div class="container-fluid">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only"> Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                                <a class="navbar-brand page-scroll" href="#page-top"></a>
-                                <a class="navbar-brand" href="index.php">Kadab's Cabs</a>
-                                <?php 
-                                if (! isset($_SESSION["custsucess"]) ) 
-                                {
-                                ?>
-                                 <a class="navbar-brand" href="book.php">Book</a>
-                                <?php
-                                }
-                                else
-                                 {
-                                ?>
-                                <a class="navbar-brand" href="custbook.php">Book</a>
-                                <?php
-                                 } 
-                                 ?>
-                                
-                                <a class="navbar-brand" href="customerlogin.php">Customer Login</a>
-                                <a class="navbar-brand" href="dispatcherlogin.php">Dispatcher Login</a>
-                                <a class="navbar-brand" href="registration.php">Register</a>
-                                <a class="navbar-brand page-scroll">
-                                <?php  
-                                //check for session
-                            if ( isset($_SESSION["error"]) ) 
-                            {  
-                            unset($_SESSION["error"]); 
-                            } 
-                            if ( isset($_SESSION["success"]) ) 
-                            { 
-                            echo $_SESSION["success"];
-
-                            }
-                            if ( isset($_SESSION["custsucess"]) ) 
-                            { 
-
-                            echo $_SESSION["custsucess"];
-                            }  
-?>
-</a>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
-                    <li>
-                        <a class="page-scroll" href="about.php">About</a>
-                    </li>
-                    <?php
-
-                        if(!isset($_SESSION["name"])) 
-                        { //if session not found
-                        ?>
-                        <li>
-                        <a class="page-scroll"href="dispatcherlogin.php">  Dispatcher Sign in       </a>
-                                            </li>
-                                            <li>
-
-                        <a class="page-scroll"href="customerlogin.php">  Customer Sign in       </a>
-                                            </li>
-
-
-                        <?php 
-                        }
-                        else
-                        { //if session was vaild
-                        ?>
-                         <li>
-
-                        <a class="page-scroll"href="signout.php">Sign out     </a>
-                                            </li>
-
-                        <?php
-                        }
-                        ?>
-                    
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container-fluid -->
-    </nav>
 
     <header class="login-bg">
         <div class="header-content">
@@ -148,46 +11,45 @@ session_start();
 			
 			<div class="content-inner-holder">
 						<?php
-require_once("DB/DB.php");
-//get the userinput from the form 
-$firstname = $_POST['inputfirstname'];
-$lastname = $_POST['inputlastname'];
-$pickup = $_POST['inputpickup'];
-$dropoff = $_POST['inputdropoff'];
-$mobile = $_POST['inputphone'];
-$datetime = $_POST['inputdatetime'];
-$payment= $_POST['Payment'];
-$passengers= $_POST['inputpassengers'];
+            require_once("DB/DB.php");
+            //get the userinput from the form 
+            $firstname = $_POST['inputfirstname'];
+            $lastname = $_POST['inputlastname'];
+            $pickup = $_POST['inputpickup'];
+            $dropoff = $_POST['inputdropoff'];
+            $mobile = $_POST['inputphone'];
+            $datetime = $_POST['inputdatetime'];
+            $payment= $_POST['Payment'];
+            $passengers= $_POST['inputpassengers'];
 
-//$passengers= $_POST['inputpassengers'];
-// To protect MySQL injection 
-$firstname= stripslashes($firstname);
-$lastname= stripslashes($lastname);
-$firstname= mysql_real_escape_string($firstname);
-$lastname= mysql_real_escape_string($lastname);
-$pickup= stripslashes($pickup);
-$dropoff= stripslashes($dropoff);
-$pickup= mysql_real_escape_string($pickup);
-$dropoff= mysql_real_escape_string($dropoff);
-$mobile= stripslashes($mobile);
-$mobile= mysql_real_escape_string($mobile);
-$datetime= stripslashes($datetime);
-$datetime= mysql_real_escape_string($datetime);
-$payment= stripslashes($payment);
-$payment= mysql_real_escape_string($payment);
-$passengers= stripslashes($passengers);
-$passengers= mysql_real_escape_string($passengers);
+            // To protect MySQL injection 
+            $firstname= stripslashes($firstname);
+            $lastname= stripslashes($lastname);
+            $firstname= mysql_real_escape_string($firstname);
+            $lastname= mysql_real_escape_string($lastname);
+            $pickup= stripslashes($pickup);
+            $dropoff= stripslashes($dropoff);
+            $pickup= mysql_real_escape_string($pickup);
+            $dropoff= mysql_real_escape_string($dropoff);
+            $mobile= stripslashes($mobile);
+            $mobile= mysql_real_escape_string($mobile);
+            $datetime= stripslashes($datetime);
+            $datetime= mysql_real_escape_string($datetime);
+            $payment= stripslashes($payment);
+            $payment= mysql_real_escape_string($payment);
+            $passengers= stripslashes($passengers);
+            $passengers= mysql_real_escape_string($passengers);
 
-             $timecheck=mysql_query("select SYSDATE()");
-                 while($time= mysql_fetch_array($timecheck))
-               {
-                if($datetime >= $time[0])
+                         $timecheck=mysql_query("select SYSDATE()");
+                             while($time= mysql_fetch_array($timecheck))
+                           {
+                            if($datetime >= $time[0])
 
-                    {
-$book="INSERT INTO Booking (`Firstname`,`Lastname`,`Phone`,`Passengers`,`PickupLoc`,`DropoffLoc`,`Payment`,`BookingTime`)
-					 VALUES ('$firstname','$lastname','$mobile','$passengers','$pickup','$dropoff','$payment','$datetime')";
+                                {
+            $book="INSERT INTO Booking (`Firstname`,`Lastname`,`Phone`,`Passengers`,`PickupLoc`,`DropoffLoc`,`Payment`,`BookingTime`)
+            					 VALUES ('$firstname','$lastname','$mobile','$passengers','$pickup','$dropoff','$payment','$datetime')";
 
-	if(mysql_query($book)== TRUE)
+            	if(mysql_query($book)== TRUE)
 				{
                     $bookinginfo=mysql_query("select * from Booking where Firstname='$firstname'
                         AND PickupLoc='$pickup'");
--- navbar.php
+++ navbar.php
@@ -1,61 +1,140 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Navigation bar</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-<!-- Optional Bootstrap theme -->
-<link rel="stylesheet" href="css/bootstrap-theme.min.css">
-</head>
-<body>
-	<div class="container">
-	<header class="navbar-inverse" role="banner">
-		<div class="container">
-	<navrole="navigation" style="middle">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">Register</a>
-    </div>
+<?php
+session_start();
+                        if ( isset($_SESSION["error"]) ) 
+                            {  
+                            unset($_SESSION["error"]); 
+                            } 
+                            if ( isset($_SESSION["success"]) ) 
+                            { 
+                             $_SESSION["success"];
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Login <span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Book</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Register <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Register as dispatcher</a></li>
-            <li><a href="#">Register as Customer</a></li>
-          </ul>
-        </li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Menu <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Book</a></li>
-            <li><a href="#">Register as customer</a></li>
-            <li><a href="#">Register as Dispatcher</a></li>
-            <li><a href="#">Book A Taxi</a></li>
-            <li><a href="#">About Us</a></li>
-            <li><a href="#">Register</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div><!-- /.navbar-collapse -->
-</nav>
-</div>
-</header>
-    <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-</div>
-</body>
-</html>                                		 
+                            }
+                            if ( isset($_SESSION["custsucess"]) ) 
+                            { 
+
+                            $_SESSION["custsucess"];
+                            }  
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Creative - Start Bootstrap Theme</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+
+    <!-- Custom Fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css" type="text/css">
+
+    <!-- Plugin CSS -->
+    <link rel="stylesheet" href="css/animate.min.css" type="text/css">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/creative.css" type="text/css">
+
+</head>
+
+<body id="page-top">
+
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only"> Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                        <a class="navbar-brand page-scroll" href="#page-top"></a>
+                        <a class="navbar-brand" href="index.php">Kadab's Cabs</a>
+                                <?php 
+                                if (! isset($_SESSION["custsucess"]) ) 
+                                {
+                                ?>
+                                 <a class="navbar-brand" href="book.php">Book</a>
+                                <?php
+                                }
+                                else
+                                 {
+                                ?>
+                                <a class="navbar-brand" href="custbook.php">Book</a>
+                                <?php
+                                 } 
+                                 ?>
+                        
+                <a class="navbar-brand" href="customerlogin.php">Customer Login</a>
+                                <a class="navbar-brand" href="dispatcherlogin.php">Dispatcher Login</a>
+                                <a class="navbar-brand" href="registration.php">Register</a>
+                                <a class="navbar-brand page-scroll">
+                                <?php  
+                                //check for session
+                            if ( isset($_SESSION["error"]) ) 
+                            {  
+                            unset($_SESSION["error"]); 
+                            } 
+                            if ( isset($_SESSION["success"]) ) 
+                            { 
+                            echo $_SESSION["success"];
+
+                            }
+                            if ( isset($_SESSION["custsucess"]) ) 
+                            { 
+
+                            echo $_SESSION["custsucess"];
+                            }  
+?>
+</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a class="page-scroll" href="about.php">About</a>
+                    </li>
+                    <?php
+
+                        if(!isset($_SESSION["name"])) 
+                        { //if session not found
+                        ?>
+                        <li>
+                        <a class="page-scroll"href="dispatcherlogin.php">  Dispatcher Sign in       </a>
+                                            </li>
+                                            <li>
+
+                        <a class="page-scroll"href="customerlogin.php">  Customer Sign in       </a>
+                                            </li>
+
+
+                        <?php 
+                        }
+                        else
+                        { //if session was vaild
+                        ?>
+                         <li>
+
+                        <a class="page-scroll"href="signout.php">Sign out     </a>
+                                            </li>
+
+                        <?php
+                        }
+                        ?>
+                    
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
