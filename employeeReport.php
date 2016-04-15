3123e209e6155d45ed770907148cf83c4c59660f
aheikkala <d15123109@mydit.ie>
2016-04-15 07:08:31 +0100

    file created

---
+++ employeeReport.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Employee Report</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+    <div class="container">
+			
+        
+		<h2>All Drivers -
+		<script language="javascript"> 
+		var today = new Date(); 
+		document.write(today.toDateString()); 
+		</script>
+		</h2>
+		
+		
+        <table class="table">
+		
+		 <thead>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Licence Plate</th>
+                </tr>
+            </thead>
+			
+			<tbody>
+		
+		
+		 
+		
+		
+		<?php
+					  		require_once("DB/DB.php");
+
+ 
+			$sql = "select FirstName,LastName, Licenseplate from taxi";
+
+
+             $rs_result = mysql_query($sql); //run the query
+
+
+            while($row = mysql_fetch_assoc($rs_result))
+                {
+				echo("<tr><td>"); 
+				echo($row['FirstName']);
+				echo("</td><td>");
+				echo($row['LastName']);
+				echo("</td><td>");
+				echo($row['Licenseplate']);
+				echo("</td></tr>");
+				}
+			
+				
+		 
+             
+           
+            
+			?>
+			</tbody>
+        </table>
+
+        <!--<input type="button" value="Print this page" onclick="window.print()"> -->
+		
+	
+    </div>
+	
+	<script type="text/javascript">
+		$(window).load(function(){
+        $('#popUpWindow').modal('show');
+		
+		});
+	</script>
+		
+		
+	
+	<div class="modal fade" id="popUpWindow">
+        <div class="modal-dialog">
+            <div class="modal-content">
+
+                <!-- header -->
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h3 class="modal-title">Print This Report?</h3>
+                </div>
+
+                
+
+                <!-- button -->
+                <div class="modal-footer">
+                    <button id="printButton" class="btn btn-primary btn-block" data-dismiss="modal">OK</button>
+                </div>
+
+            </div>
+        </div>
+    </div>
+	
+
+	
+	<script type="text/javascript">
+		document.getElementById("printButton").onclick = function () {
+		$("#popUpWindow").modal("hide")
+		window.print();
+		};
+	</script>
+	
+
+
+</body>
+</html>
---
+++ reports.php
@@ -0,0 +1,80 @@
+<?php
+require_once("navbar.php")
+?>
+    <header class="login-bg">
+        <div class="header-content">
+		<img border="0" alt="Kadab"  src="img/icon.png">
+		
+			<div class="header-content-inner_dash">
+				<!--<div class="navbar"></div>-->
+
+				<div id="menu-left">
+					<a href="darshboard.php">Dashboard</a>
+					<a href="#about">Assign Taxi</a>
+					<a href="#about">Phone Bookings</a>
+					<a href="#about">Reports</a>
+					<a href="availablebookings.php">Available Bookings</a>
+					<a href="#about">Find Out More</a>
+
+				</div>
+			
+				<div class="header-content-inner_3" >
+					
+					
+				  <h2>Print Reports</h2>
+				
+					<button id="myButton" class="btn btn-primary btn-block" >Current Empoyees</button>
+
+						<script type="text/javascript">
+						document.getElementById("myButton").onclick = function () {
+						window.open('employeeReport.php', '_blank');
+						};
+						</script>
+					
+					<br><button id="myButton2" class="btn btn-primary btn-block" >Report 2</button></br>
+
+						<script type="text/javascript">
+						document.getElementById("myButton2").onclick = function () {
+						location.href = "index.php";
+						};
+						</script>
+					
+					<button id="myButton3" class="btn btn-primary btn-block" >Report 3</button>
+
+						<script type="text/javascript">
+						document.getElementById("myButton3").onclick = function () {
+						location.href = "index.php";
+						};
+						</script>
+						
+					<br><button id="myButton4" class="btn btn-primary btn-block" >Report 4</button></br>
+
+						<script type="text/javascript">
+						document.getElementById("myButton4").onclick = function () {
+						location.href = "index.php";
+						};
+						</script>
+						
+
+						
+				
+				</div>
+   
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="js/jquery.easing.min.js"></script>
+    <script src="js/jquery.fittext.js"></script>
+    <script src="js/wow.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="js/creative.js"></script>
+
+</body>
+
+</html>
